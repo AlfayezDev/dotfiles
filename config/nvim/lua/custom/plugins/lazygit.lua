4649f022a81f1a713c1b5e6aaf9826c3dfc12781
alfayezDev <mohammed@alfayez.dev>
2026-04-27 18:49:03 +0300

    fixes to treesitter

--- config/nvim/lua/custom/plugins/lazygit.lua
+++ config/nvim/lua/custom/plugins/lazygit.lua
@@ -15,6 +15,7 @@ return {
     { '<leader>gg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
   },
   config = function()
+    vim.g.lazygit_use_neovim_remote = 1
     require('telescope').load_extension 'lazygit'
   end,
 }
